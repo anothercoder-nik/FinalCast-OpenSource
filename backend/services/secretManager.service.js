296b6397a052dd9556291f1c7e849d247f4ede60
Gupta-02 <Gupta-02@users.noreply.github.com>
2026-01-24 11:26:39 +0530

    Implemented

--- backend/services/secretManager.service.js
+++ backend/services/secretManager.service.js
@@ -13,7 +13,7 @@ class SecretManager {
 
   encrypt(text) {
     const iv = crypto.randomBytes(IV_LENGTH);
-    const cipher = crypto.createCipher('aes-256-cbc', ENCRYPTION_KEY);
+    const cipher = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return iv.toString('hex') + ':' + encrypted;
@@ -23,7 +23,7 @@ class SecretManager {
     const textParts = encryptedText.split(':');
     const iv = Buffer.from(textParts.shift(), 'hex');
     const encrypted = textParts.join(':');
-    const decipher = crypto.createDecipher('aes-256-cbc', ENCRYPTION_KEY);
+    const decipher = crypto.createDecipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
